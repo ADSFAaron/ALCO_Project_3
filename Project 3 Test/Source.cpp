d989a66071fd298a9daed2b38ce69fb432939b7e
s1073327 <51864126+s1073327@users.noreply.github.com>
2020-05-24 23:50:17 +0800

    Add files via upload

--- Project 3 Test/Source.cpp
+++ Project 3 Test/Source.cpp
@@ -1,134 +1,242 @@
 #include<iostream>
 #include<iomanip>
 #include<fstream>
+#include <sstream>
 #include<string>
+#include <vector>
 using namespace std;
 
-int reg[32] = {}; //register
-int labtime = 0;
-int beqtime = 0;
-int now = 0;
-
-struct beq {
+struct doPrediction
+{
 	int entry;
-	string r1;
-	string r2;
+	string rs1;
+	string rs2;
 	string label;
-}_beq[50]; //存入每個beq
+};
 
-struct label {
+struct label
+{
 	string name;
 	int pos;
-}_lab[50];
+};
 
-struct predict {
-	string state[4] = { "SN","SN","SN","SN" };
-	string his[2] = { "N","N" }; //his[0]=上個 his[1]=上上個
-	int pred1 = 0; //上個
-	int pred2 = 0; //上上個
-	string pred = "N"; //當前預測值
-	int miss = 0; //預測錯誤次數
-}_pred[50];
+struct predict
+{
+	string state[4] = { "SN","SN","SN","SN" };	//預設的4個state
+	string his[2] = { "N","N" };								//his[0] = 上個 his[1] = 上上個
+	int pred1 = 0;
+	int pred2 = 0;
+	string pred = "N";											//當前預測值
+	int miss = 0;													//預測錯誤次數
+};
 
-void li(string s1, string c) {
-	//cout << "li " << s1 << "," << c << endl;
-	int r = atoi(s1.substr(1, s1.length() - 1).c_str()); //擷取去掉r之後的數字
-	int num = atoi(c.c_str());
-	reg[r] = num;  //把當前的reg的值放入reg裡
-	//cout << reg[0] << reg[1] << reg[2];
+struct instruct
+{
+	string name;		//Instruction Name
+	string reg1;		//Instruction  Register Destination
+	string reg2;		//(Optional) Instruction Register
+	string imm;		//Instruction Immediate
+};
+
+doPrediction pred[100];
+predict State[100];
+vector<instruct> implement;
+vector<label> Label;
+int reg[32] = {};
+int rowPos = 0;
+int entry = 0;
+
+bool label_detected(string& input)
+{
+	//找到label 
+	if (input.find(':') != string::npos)
+	{
+		input[input.size() - 1] = '\0';
+		input.resize(input.size() - 1);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
-void addi(string s1, string s2, string c) {
-	//cout << "addi " << s1 << "," << s2 << "," << c << endl;
-	int r1 = atoi(s1.substr(1, s1.length() - 1).c_str());
-	int r2 = atoi(s2.substr(1, s2.length() - 1).c_str());
-	int num = atoi(c.c_str());
+void li(string rs1, string imm)
+{
+	int r = atoi(rs1.substr(1, rs1.length() - 1).c_str());	//將rd 改成數字，從register 取值
+	int num = atoi(imm.c_str());
+	reg[r] = num;
+}
+
+void addi(string rd, string rs, string imm)
+{
+	int r1 = atoi(rd.substr(1, rd.length() - 1).c_str());
+	int r2 = atoi(rs.substr(1, rs.length() - 1).c_str());
+	int num = atoi(imm.c_str());
 	reg[r1] = reg[r2] + num;
-	//cout << reg[0] << reg[1] << reg[2];
 }
 
-void beq(string s1, string s2, string s3) {
-	string nowstate;
-	int beqno = 0;
-	int statenum;
-	string result = "T";
-	for (int i = 0; i < beqtime; i++) {
-		if (_beq[i].r1 == s1 && _beq[i].r2 == s2 && _beq[i].label == s3)
-			beqno = i;
+void prediction(int rs1, int rs2, string label)
+{
+	string nowstate;		//判斷目前在哪個state
+	int beqno = 0;				//判斷是再跑which beq
+	int statenum;				//判斷第幾個state
+	string result = "T";	//state實際結果
+	int beqCount = 0;
+
+	//找instruction中所有beq
+	for (int i = 0; i < implement.size(); ++i)
+	{
+		if (implement[i].name == "beq")
+		{
+			pred[beqCount].entry = i % entry;
+			pred[beqCount].label = implement[i].imm;
+			pred[beqCount].rs1 = implement[i].reg1;
+			pred[beqCount].rs2 = implement[i].reg2;
+			++beqCount;
+		}
 	}
-	cout << "entry: " << _beq[beqno].entry << setw(7) << "beq " << s1 << "," << s2 << "," << s3 << endl;
 
-	int r1 = atoi(s1.substr(1, s1.length() - 1).c_str());
-	int r2 = atoi(s2.substr(1, s2.length() - 1).c_str());
-	int predtime = _beq[beqno].entry / 2 - 1;
-	if (_pred[predtime].his[0] == "N") //判斷歷史條件並設定好
-		_pred[predtime].pred1 = 0;
-	else
-		_pred[predtime].pred1 = 1;
-	if (_pred[predtime].his[1] == "N")
-		_pred[predtime].pred2 = 0;
-	else
-		_pred[predtime].pred2 = 1;
+	int predtime = 0;
 
-	if (_pred[predtime].pred2 == 0 && _pred[predtime].pred1 == 0) //設定現在的state
+	//找出所有beq
+	for (int i = 0; i < beqCount; ++i)
+	{
+		if (pred[i].label == label)
+		{
+			predtime = pred[i].entry;
+
+			//印出beq對應的entry
+			cout << "entry: " << predtime << setw(7) << "beq " << pred[i].rs1 << "," << pred[i].rs2 << "," << label << endl;
+			break;
+		}
+	}
+
+	//判斷 2-bit history 為哪個
+	if (State[predtime].his[0] == "N")
+		State[predtime].pred1 = 0;
+	else
+		State[predtime].pred1 = 1;
+
+	if (State[predtime].his[1] == "N")
+		State[predtime].pred2 = 0;
+	else
+		State[predtime].pred2 = 1;
+
+	//按照2-bit history設定目前state
+	if (State[predtime].pred2 == 0 && State[predtime].pred1 == 0)
+	{
 		nowstate = "00";
-	else if (_pred[predtime].pred2 == 1 && _pred[predtime].pred1 == 0)
-		nowstate = "10";
-	else if (_pred[predtime].pred2 == 0 && _pred[predtime].pred1 == 1)
+		statenum = 0;
+	}
+	else if (State[predtime].pred2 == 0 && State[predtime].pred1 == 1)
+	{
 		nowstate = "01";
+		statenum = 1;
+	}
+	else if (State[predtime].pred2 == 1 && State[predtime].pred1 == 0)
+	{
+		nowstate = "10";
+		statenum = 2;
+	}
 	else
+	{
 		nowstate = "11";
-	statenum = _pred[predtime].pred2 * 2 + _pred[predtime].pred1;
-	if (_pred[predtime].state[statenum] == "SN" || _pred[predtime].state[statenum] == "WN")
-		_pred[predtime].pred = "N";
+		statenum = 3;
+	}
+
+	//預測是N or T
+	if (State[predtime].state[statenum] == "SN" || State[predtime].state[statenum] == "WN")
+		State[predtime].pred = "N";
 	else
-		_pred[predtime].pred = "T";
-	cout << "(" << nowstate << ", " << _pred[predtime].state[0] << ", " << _pred[predtime].state[1] << ", " << _pred[predtime].state[2] << ", " << _pred[predtime].state[3] << ")" << _pred[predtime].pred << " ";
-	if (reg[r1] == reg[r2]) {
+		State[predtime].pred = "T";
+
+	//輸出結果
+	cout << "(" << nowstate << ", " << State[predtime].state[0] << ", " << State[predtime].state[1] << ", " << State[predtime].state[2] << ", " << State[predtime].state[3] << ") "
+		<< State[predtime].pred << " ";
+
+	//實際beq結果
+	if (reg[rs1] == reg[rs2])
+	{
 		result = "T";
-		if (result == _pred[predtime].pred) {//改變STATE裡的狀態
-			if (_pred[predtime].state[statenum] == "WT")
-				_pred[predtime].state[statenum] = "ST";
+
+		//改變state裡的狀態
+		if (result == State[predtime].pred)
+		{
+			if (State[predtime].state[statenum] == "WT")
+				State[predtime].state[statenum] = "ST";
 		}
-		else {
-			if (_pred[predtime].state[statenum] == "SN")
-				_pred[predtime].state[statenum] = "WN";
-			else if (_pred[predtime].state[statenum] == "WN")
-				_pred[predtime].state[statenum] = "WT";
-			_pred[predtime].miss++;
-		}
-		cout << result << setw(18) << "misprediction: " << _pred[predtime].miss << endl;
-		for (int i = 0; i < labtime; i++) {
-			if (_lab[i].name == s3) {
-				now = _lab[i].pos - 1;
-			}
+		else
+		{
+			if (State[predtime].state[statenum] == "SN")
+				State[predtime].state[statenum] = "WN";
+			else if (State[predtime].state[statenum] == "WN")
+				State[predtime].state[statenum] = "WT";
+			State[predtime].miss++;
 		}
 	}
-	else {
+	else
+	{
 		result = "N";
-		if (result == _pred[predtime].pred) {//改變STATE裡的狀態
-			if (_pred[predtime].state[statenum] == "WN")
-				_pred[predtime].state[statenum] = "SN";
+
+		//改變state狀態
+		if (result == State[predtime].pred)
+		{
+			if (State[predtime].state[statenum] == "WN")
+				State[predtime].state[statenum] = "SN";
 		}
-		else {
-			if (_pred[predtime].state[statenum] == "ST")
-				_pred[predtime].state[statenum] = "WT";
-			else if (_pred[predtime].state[statenum] == "WT")
-				_pred[predtime].state[statenum] = "WN";
-			_pred[predtime].miss++;
+		else
+		{
+			if (State[predtime].state[statenum] == "ST")
+				State[predtime].state[statenum] = "WT";
+			else if (State[predtime].state[statenum] == "WT")
+				State[predtime].state[statenum] = "WN";
+
+			State[predtime].miss++;
 		}
-		cout << result << setw(18) << "misprediction: " << _pred[predtime].miss << endl << endl;
 	}
-	_pred[predtime].his[1] = _pred[predtime].his[0];
-	_pred[predtime].his[0] = result;
-	//cout << reg[0] << reg[1] << reg[2];
+
+	cout << result << setw(18) << "misprediction: " << State[predtime].miss << endl << endl;
+
+	State[predtime].his[1] = State[predtime].his[0];
+	State[predtime].his[0] = result;
 }
 
-int main() {
+void beq(string rs1, string rs2, string label)
+{
+	int r1 = atoi(rs1.substr(1, rs1.length() - 1).c_str());
+	int r2 = atoi(rs2.substr(1, rs2.length() - 1).c_str());
+
+	prediction(r1, r2, label);
+
+	if (reg[r1] == reg[r2] && label == "End")
+	{
+		rowPos = -2;
+		return;
+	}
+
+	//if(r1 == r2) 
+	//		jump to Label
+	for (int i = 0; i < Label.size(); ++i)
+	{
+		if (reg[r1] == reg[r2] && label == Label[i].name)
+		{
+			rowPos = Label[i].pos - 1;
+		}
+	}
+}
+
+int main()
+{
+	label tmp;
+	instruct file;
+	string input;
+	int row = 0;			//判斷輸入到第幾行
 
 	//讀RISC V code
 	ifstream infile("test.txt", ios::in);
 
+	//檔案不存在
 	if (!infile)
 	{
 		cerr << "Risc v file not found" << endl;
@@ -136,59 +244,70 @@ int main() {
 		exit(0);
 	}
 
-	string s[200], ss;
-	int entry;
-	int entrystrart = 2;
 	cout << "Input : number of entries" << endl;
 	cin >> entry;
-	int num = 0;
 
-	while (infile >> ss) {
-		for (int j = 0; j < ss.length(); j++) {
-			if (ss[j] == ',') {
-				j++;
-				num++;
+	//讀RISC V code
+	while (getline(infile, input))
+	{
+		//label存在，將label名字和行數儲存
+		if (label_detected(input))
+		{
+			tmp.name = input;
+			tmp.pos = row;
+			Label.push_back(tmp);
+		}
+		else
+		{
+			stringstream ss(input);
+
+			//分割instruction name
+			getline(ss, file.name, ' ');
+
+			if (file.name == "li")
+			{
+				getline(ss, file.reg1, ',');
+				getline(ss, file.imm, '\n');
 			}
-			s[num] += ss[j];
-		}
-		num++;
-	}
-
-	for (int i = 0; i < num; i++) {
-		if (s[i] == "beq") {
-			int tmp = i + 3;
-			_lab[labtime].name = s[tmp];
-			_beq[beqtime].r1 = s[i + 1];
-			_beq[beqtime].r2 = s[i + 2];
-			_beq[beqtime].label = s[tmp];
-			_beq[beqtime].entry = entrystrart;
-			entrystrart += 2;
-			beqtime++;
-		}
-		for (int j = 0; j < num; j++) {
-			if ((_lab[labtime].name + ':') == s[j]) {
-				_lab[labtime].pos = j;
-				labtime++;
+			else if (file.name == "addi" || file.name == "beq")
+			{
+				getline(ss, file.reg1, ',');
+				getline(ss, file.reg2, ',');
+				getline(ss, file.imm, '\n');
 			}
+			else
+			{
+				cout << "error infile \n\n";
+				system("pause");
+			}
+			++row;
+
+			//將Instruction儲存到vector中
+			implement.push_back(file);
 		}
 	}
 
-	//cout << labtime << endl; TEST
+	infile.close();
 
-/*	for (int i = 0; i < labtime; i++) {
-		cout << _lab[i].name << " " << _lab[i].pos << endl;
-	}*/
-	for (; now < num; now++) {
-		if (s[now] == "li") {
-			li(s[now + 1], s[now + 2]);
+	while (rowPos > -1)
+	{
+		//判斷是哪個instruction
+		if (implement[rowPos].name == "li")
+		{
+			li(implement[rowPos].reg1, implement[rowPos].imm);
 		}
-		else if (s[now] == "addi") {
-			addi(s[now + 1], s[now + 2], s[now + 3]);
+		else if (implement[rowPos].name == "addi")
+		{
+			addi(implement[rowPos].reg1, implement[rowPos].reg2, implement[rowPos].imm);
 		}
-		else if (s[now] == "beq") {
-			beq(s[now + 1], s[now + 2], s[now + 3]);
+		else if (implement[rowPos].name == "beq")
+		{
+			beq(implement[rowPos].reg1, implement[rowPos].reg2, implement[rowPos].imm);
 		}
+
+		++rowPos;
 	}
-	cout << reg[0] << reg[1] << reg[2];
+
 	system("pause");
+	return 0;
 }
